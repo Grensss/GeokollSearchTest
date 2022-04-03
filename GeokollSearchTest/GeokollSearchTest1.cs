3303b959bccd796205e0551c5fc61d93c23d6025
Arturs Grens <grenartur@gmail.com>
2022-04-03 23:24:22 +0300

    End

--- GeokollSearchTest/GeokollSearchTest1.cs
+++ GeokollSearchTest/GeokollSearchTest1.cs
@@ -1,5 +1,7 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 
 namespace GeokollSearchTest
 {
@@ -9,15 +11,15 @@ namespace GeokollSearchTest
         private IWebDriver driver;
         private readonly By SearchInputSpace = By.XPath("//input[@name = 'q']");
         private readonly By SearchAfterInputButton = By.XPath("//i[@class = 'fa fa-search']");
-        private readonly By ProductName = By.XPath("//h5[@class = 'card-title mw-300']");
-        
 
+        private readonly By Title = By.XPath("//h5[contains(text(),'Grey')]");
+        
         private const string SearchProduct = "Biogel No Limits Grey 25 kg";
         
         [SetUp]
         public void Setup()
         {
-            driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+            driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://geokoll.lv/lv");
             driver.Manage().Window.Maximize();
         }
@@ -26,16 +28,15 @@ namespace GeokollSearchTest
         public void Test1()
         {
             var SearchSpace = driver.FindElement(SearchInputSpace);
-            SearchSpace.Click();
             SearchSpace.SendKeys(SearchProduct);
             
             var SearchButton = driver.FindElement(SearchAfterInputButton);
             SearchButton.Click();
-           
-            var ProductNameClick = driver.FindElement(ProductName);
-            ProductNameClick.Click();
 
-            driver.FindElement(By.Id("layout-content")).Text.Contains(SearchProduct);
+            var TitleText = driver.FindElement(Title);
+            
+            Assert.AreEqual(SearchProduct ,TitleText.Text);
+            
         }
         [TearDown]
 
